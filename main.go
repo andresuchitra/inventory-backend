4c57e712f80c1fbcf66e7e28462b55753702cc34
Andre Suchitra <andre.suchitra@mekari.com>
2020-11-18 09:03:15 +0700

    tls

--- main.go
+++ main.go
@@ -33,5 +33,5 @@ func main() {
 	originsOK := handlers.AllowedOrigins([]string{"*"})
 	methodsOK := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "DELETE", "PUT"})
 
-	log.Fatal(http.ListenAndServe(":3001", handlers.CORS(originsOK, methodsOK)(loggedRouter)))
+	log.Fatal(http.ListenAndServeTLS(":3001", os.Getenv(("CRT_FILE"), os.Getenv("KEY_FILE"), handlers.CORS(originsOK, methodsOK)(loggedRouter)))
 }
